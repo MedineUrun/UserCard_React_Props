383d5cd0e51f1177a64957d291ced310d11b17ea
MedineUrun <52495729+MedineUrun@users.noreply.github.com>
2022-04-12 09:07:42 +0300

    medine

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import profile1 from './image/images1.jpg'
+import profile2 from './image/images2.jpg'
+import profile3 from './image/images3.jpg'
+import SingleComment from "./SingleComment";
+import UserCard from "./UserCard";
+
+const App = () => {
+
+    return (
+        <div className="ui comments">
+            <UserCard>
+                hgsadghdsahgdshgj
+            </UserCard>
+            <UserCard>
+            <SingleComment
+             name='Alex' 
+             date='Today at 5:00 PM' 
+             text='İt is amazing'
+             image={profile1}
+             />
+            </UserCard>
+            <UserCard>
+            <SingleComment 
+            name='Sarah' 
+            date='Today at 20:00 PM'
+            text='Beautiful'
+            image={profile1}
+            />
+            </UserCard>
+            <UserCard>
+            <SingleComment 
+            name='Leonel' 
+            date='Today at 8:00 PM' 
+            text='Thanks'
+            image={profile3}
+            />
+            </UserCard>
+        </div>
+    )
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
